adc00b0b146535239139c88fc44a5367d38381e9
ThomasP32 <thomas.perron-duveau@polymtl.ca>
2025-03-23 20:17:48 -0400

    fix partieC

--- .github/workflows/myscript.py
+++ .github/workflows/myscript.py
@@ -1,3 +1,8 @@
 import os
-os.system("git bisect start $badhash $goodhash")
-os.system("git bisect run test")
+BAD_HASH = "c1a4be04b972b6c17db242fc37752ad517c29402"
+GOOD_HASH = "e4cfc6f77ebbe2e23550ddab682316ab4ce1c03c"
+
+os.system(f"git bisect start {BAD_HASH} {GOOD_HASH}")
+exit_code = os.system("git bisect run test")
+
+sys.exit(exit_code)
